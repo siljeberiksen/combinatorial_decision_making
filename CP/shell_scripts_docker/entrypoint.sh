d36d73b862fbc12de5bc153f9915bd68c118d0b9
Silje Birkeland Eriksen <siljeberiksen@gmail.com>
2024-07-07 12:19:18 +0200

    Changed way of entrypoint

--- CP/shell_scripts_docker/entrypoint.sh
+++ CP/shell_scripts_docker/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-if [ "$1" = "single" ]; then
+TYPE=$1
+if [ "$TYPE" = "single" ]; then
     # Assign the arguments to variables
     MODEL_FILE=$2
     INSTANCE_FILE=$3
     RESULT_FILE=$4
     SOLVER=$5
     exec ./CP/shell_scripts_docker/run_single_model.sh "$MODEL_FILE" "$INSTANCE_FILE" "$RESULT_FILE" "$SOLVER"
-elif [ "$1" = "all" ]; then
+elif [ "$TYPE" = "all" ]; then
     exec ./CP/shell_scripts_docker/run_all_models.sh
 else
     echo "Usage: $0 {single|all}"
